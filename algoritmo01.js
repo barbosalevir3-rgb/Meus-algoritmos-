471f52749f17f006337789ddc568870770f6ecc3
Levir Barbosa <barbosalevir3@gmail.com>
2026-03-20 09:48:01 -0400

    Criar algoritmo01.js

---
+++ algoritmo01.js
@@ -0,0 +1,6 @@
+let ano, aniversário, meses, dias
+ano = prompt("Ano de nascimento")
+aniversário = 2026 - ano
+meses = aniversário * 12
+dias = aniversário * 12 
+alert(`anos: ${aniversario} de vida\nMeses: ${meses}\nDias: ${dias}`)

98fcb90f50b9bffff47479c5a3a14ed116267655
Levir Barbosa <barbosalevir3@gmail.com>
2026-03-20 10:12:40 -0400

    Criar livros.js

---
+++ livros.js
@@ -0,0 +1,5 @@
+//observação: este algoritmo pede o número de páginas do livro que você vai iniciar a leitura, junto com os dias de leitura. Por exemplo: vou ler um livro de 80 páginas em 4 dias. 
+
+let páginas livro = parseInt(prompt("número de páginas")
+let dias = parseInt(prompt("dias de leitura")
+alert(`paginas por dia ${Matheus.cell(páginas livro/dias)}`)

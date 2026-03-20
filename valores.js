1cb7ef00dcee5fd0c7bde6b21b9351f97ba6c18a
Levir Barbosa <barbosalevir3@gmail.com>
2026-03-20 10:49:31 -0400

    Criar valores.js

---
+++ valores.js
@@ -0,0 +1,5 @@
+//algoritmo pede dois valores. como você faria isso??
+ 
+let num1 = parseInt(prompt("Digite o primeiro valor"))
+let num2 = parseInt(prompt(Digite o segundo valor"))
+alert((num1==num2)?num1+num2:num1*num2)

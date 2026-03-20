45d05b6eaf03c4f3ca5039ab2485da12636cb232
Levir Barbosa <barbosalevir3@gmail.com>
2026-03-20 10:18:23 -0400

    Criar distância.js

---
+++ distância.js
@@ -0,0 +1,7 @@
+// algoritmo vai imprimir a distância percorrida até um determinado ponto. exemplo: João todos dias vai para escola, mas um certo dia ele resolveu calcular, distância, km, semana e aula.
+
+let distancia, km, semana, aula
+km = prompt("Distância percorrida por dia")
+dias = 8*6
+semana = 5*8
+alert(`distancia:${km} percorrida\nsemana:${semana}`)

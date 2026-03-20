a64c697cc4c9b42e1f0d17ea79e8b5283bacc1de
Levir Barbosa <barbosalevir3@gmail.com>
2026-03-20 10:04:40 -0400

    Criar meu-teste.js

---
+++ meu-teste.js
@@ -0,0 +1,8 @@
+// esse meu algoritmo é um teste usando vetores.
+
+let n = Number(prompt("Quantas transferências deseja registrar?"))
+const transferencia = []
+for (let i = 0; i < n; i++) {
+  
+}
+alert("Transferência concluída!\nValores digitados: " + transferencia)

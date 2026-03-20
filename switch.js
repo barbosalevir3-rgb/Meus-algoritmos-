9388ba338dcbeb3e9dc767f6050735126c7efcc6
Levir Barbosa <barbosalevir3@gmail.com>
2026-03-20 10:01:03 -0400

    Criar switch.js

---
+++ switch.js
@@ -0,0 +1,17 @@
+let frutas = prompt("0.Mama-cadela\n1.Fruta-pão\n2.Bacuri\n3.Sorva\n4.Biribá\n5.Seriguela")
+switch(frutas){
+    case '0':
+        alert("Brosimum gaudichaudii Trécul");break;
+    case '1':
+        alert("Artocarpus artilis");break;
+    case '2':
+        alert("Platonia insignis");break;
+    case '3':
+        alert("Sorbus domestica");break;
+    case '4':
+        alert("Annona mucosa");break;
+    case '5':
+        alert("Spondias purpurea");break;
+    default:
+        alert("Fruta não encontrada")
+}

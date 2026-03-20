22a65faf7f804a9055031a3814d97bd9143b22b4
Levir Barbosa <barbosalevir3@gmail.com>
2026-03-20 09:51:55 -0400

    Criar algoritmo03.js

---
+++ algoritmo03.js
@@ -0,0 +1,4 @@
+let idade1 = parseInt(prompt("Digite a primeira idade"))
+let idade2 = parseInt(prompt("Digite a segunda idade"))
+let idade3 = parseInt(prompt("Digite a terceira idade"))
+alert(`idade: ${(idade1 + idade2 + idade3)\3}`)

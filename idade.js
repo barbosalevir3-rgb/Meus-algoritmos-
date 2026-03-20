bfdf38f0fcd27950af521d308e9142daa7ef4f23
Levir Barbosa <barbosalevir3@gmail.com>
2026-03-20 09:42:13 -0400

    Criar idade.js

---
+++ idade.js
@@ -0,0 +1,5 @@
+// esse algoritmo pede seu ano de nascimento e subtrai com ano atual entrega sua idade. 
+
+let idade = prompt("Digite seu ano de nascimento")
+idade = 2025 - idade
+alert("Parabéns, você tem " + idade + " anos de vida!")

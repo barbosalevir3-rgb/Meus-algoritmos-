5d7236db8a5ccc2fd18db41029be0816521dac01
Levir Barbosa <barbosalevir3@gmail.com>
2026-03-20 09:44:42 -0400

    Criar meses.js
    divirtam-se!

---
+++ meses.js
@@ -0,0 +1,4 @@
+// esse algoritmo pede sua idade multiplica pela quantidade de meses  e imprime seus meses de vida. 
+let idade = prompt("idade")
+let meses = idade * 12
+alert("Você tem " +meses+ " de vida")
